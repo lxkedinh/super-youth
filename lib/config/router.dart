56fd01afcece67aac21c9574285331792280d7b3
Luke Dinh <lukexdinh@gmail.com>
2025-06-18 14:34:05 -0700

    chore: adjust app routes

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:sunny_chen_project/screens/home_screen.dart';
 import 'package:sunny_chen_project/screens/login_screen.dart';
+import 'package:sunny_chen_project/screens/profile_screen.dart';
 import 'package:sunny_chen_project/screens/sign_up_screen.dart';
 import 'package:sunny_chen_project/screens/splash_screen.dart';
 
@@ -8,7 +9,13 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => SplashScreen()),
-    GoRoute(path: '/home', builder: (context, state) => HomeScreen()),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => HomeScreen(),
+      routes: [
+        GoRoute(path: 'profile', builder: (context, state) => ProfileScreen()),
+      ],
+    ),
     GoRoute(path: '/login', builder: (context, state) => LoginScreen()),
     GoRoute(path: '/signup', builder: (context, state) => SignUpScreen()),
   ],
