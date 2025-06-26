43d769cd837bcfb7955e399e0e8b9ce4dd5de306
Luke Dinh <lukexdinh@gmail.com>
2025-06-26 15:40:13 -0700

    feat: created Unit data class
    - Unit class holds app's educational content

---
+++ lib/data/unit.dart
@@ -0,0 +1,35 @@
+class Unit {
+  String name;
+  String id;
+  String description;
+  int numScenarios;
+
+  Unit(this.name, this.id, this.description, this.numScenarios);
+}
+
+final List<Unit> units = [
+  Unit(
+    "Needs and Values",
+    "needs-and-values",
+    "Helping teens define their needs and values",
+    5,
+  ),
+  Unit(
+    "Active Listening",
+    "active-listening",
+    "Helping teens improve their communication through active listening",
+    5,
+  ),
+  Unit(
+    "Active Listening",
+    "active-listening",
+    "Helping teens improve their communication through active listening",
+    5,
+  ),
+  Unit(
+    "Active Listening",
+    "active-listening",
+    "Helping teens improve their communication through active listening",
+    5,
+  ),
+];
