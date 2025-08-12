98e8a95ac5794fc9478cb6731a29d7294910b9b8
Luke Dinh <lukexdinh@gmail.com>
2025-08-11 17:40:30 -0700

    feat: completed try screen
    - form to submit user response to scenario

--- lib/screens/try_screen.dart
+++ lib/screens/try_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:sunny_chen_project/data/unit.dart';
-import 'package:sunny_chen_project/services/ai_service.dart';
+import 'package:sunny_chen_project/providers/ai_provider.dart';
 
 class TryScreen extends StatefulWidget {
   final int unitId;
@@ -13,13 +14,8 @@ class TryScreen extends StatefulWidget {
 }
 
 class _TryScreenState extends State<TryScreen> {
-  late final AIService ai;
-
-  @override
-  void initState() {
-    super.initState();
-    ai = AIService();
-  }
+  final _formKey = GlobalKey<FormState>();
+  final _responseController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +29,10 @@ class _TryScreenState extends State<TryScreen> {
       ),
       body: Center(
         child: FutureBuilder(
-          future: ai.generateContent(units[widget.unitId - 1].name),
+          future: Provider.of<AIProvider>(
+            context,
+            listen: false,
+          ).generateContent(units[widget.unitId - 1].name),
           builder: (
             BuildContext context,
             AsyncSnapshot<Map<String, dynamic>> snapshot,
@@ -44,18 +43,49 @@ class _TryScreenState extends State<TryScreen> {
                   horizontal: 20,
                   vertical: 20,
                 ),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SingleChildScrollView(
                         child: Text(
-                          snapshot.data!['response'],
+                          snapshot.data!['scenario'],
                           style: TextTheme.of(context).bodyLarge,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 400, child: TextField(maxLines: null)),
-                  ],
+                      TextFormField(
+                        maxLines: 8,
+                        controller: _responseController,
+                        decoration: InputDecoration(
+                          labelText: 'Response',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        validator: (String? response) {
+                          if (response == null || response.isEmpty) {
+                            return 'Response required.';
+                          }
+                          return null;
+                        },
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            context.push(
+                              '/unit/${widget.unitId}/feedback',
+                              extra: Map.of({
+                                'scenario': snapshot.data!['scenario'],
+                                'response': _responseController.text,
+                              }),
+                            );
+                          }
+                        },
+                        child: Text("Submit"),
+                      ),
+                    ],
+                  ),
                 ),
               );
             } else if (snapshot.hasError) {
@@ -63,7 +93,7 @@ class _TryScreenState extends State<TryScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Could not generate content. Try again.",
+                    "Could not generate scenario. Try again.",
                     style: TextTheme.of(context).bodyLarge?.apply(
                       color: Theme.of(context).colorScheme.error,
                     ),
@@ -76,7 +106,7 @@ class _TryScreenState extends State<TryScreen> {
                 spacing: 20,
                 children: [
                   Text(
-                    "Generating content",
+                    "Generating scenario",
                     style: TextTheme.of(context).displaySmall,
                   ),
                   SizedBox(
