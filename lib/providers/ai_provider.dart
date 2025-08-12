83b5b87890b601d07a465b0b0d59c60044ee1257
Luke Dinh <lukexdinh@gmail.com>
2025-08-11 17:41:06 -0700

    feat: ai provider
    - change notifier provider for ai service

---
+++ lib/providers/ai_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sunny_chen_project/services/ai_service.dart';
+
+class AIProvider extends ChangeNotifier {
+  late final AIService _ai;
+
+  AIProvider() {
+    _ai = AIService();
+  }
+
+  Future<Map<String, dynamic>> generateContent(String unitTitle) async {
+    return await _ai.generateContent(unitTitle);
+  }
+
+  Future<Map<String, dynamic>> generateFeedback({
+    required String scenario,
+    required String userResponse,
+  }) async {
+    return await _ai.generateFeedback(
+      scenario: scenario,
+      userResponse: userResponse,
+    );
+  }
+}
