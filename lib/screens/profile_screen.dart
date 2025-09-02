7da57315b6afa78610253596bf64ba1ea9bc1441
Luke Dinh <lukexdinh@gmail.com>
2025-09-02 15:21:54 -0700

    feat: editing profile info

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,6 +12,135 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool _isEditing = false;
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _usernameController = TextEditingController();
+
+  Future<void> _saveProfile() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    try {
+      AuthenticationProvider authProvider = Provider.of<AuthenticationProvider>(
+        context,
+        listen: false,
+      );
+
+      await authProvider.updateProfile(
+        username: _usernameController.text.trim(),
+        firstName: _firstNameController.text.trim(),
+        lastName: _lastNameController.text.trim(),
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        _isEditing = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Profile updated successfully")),
+      );
+    } on Exception catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  Future<void> _deleteAccount() async {
+    try {
+      final authProvider = Provider.of<AuthenticationProvider>(
+        context,
+        listen: false,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Are you sure you want to delete your account?"),
+          action: SnackBarAction(
+            label: "Yes",
+            onPressed: () async {
+              await authProvider.deleteAccount();
+              if (!mounted) return;
+              context.go('/login');
+            },
+          ),
+        ),
+      );
+    } on Exception catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  Widget _buildEditProfileSection() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        margin: EdgeInsetsGeometry.symmetric(horizontal: 40),
+        child: Column(
+          spacing: 40,
+          children: [
+            TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+              validator: (String? username) {
+                if (username == null ||
+                    username.length < 3 ||
+                    username.length > 20) {
+                  return 'Username must be between 3 and 20 characters long.';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _firstNameController,
+              decoration: const InputDecoration(labelText: 'First Name'),
+              validator: (String? firstName) {
+                if (firstName == null ||
+                    firstName.isEmpty ||
+                    firstName.length > 20) {
+                  return 'First name must be non-empty and less than 20 characters long.';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _lastNameController,
+              decoration: const InputDecoration(labelText: 'Last Name'),
+              validator: (String? lastName) {
+                if (lastName == null ||
+                    lastName.isEmpty ||
+                    lastName.length > 20) {
+                  return 'Last name must be non-empty and less than 20 characters long.';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await _saveProfile();
+              },
+              child: Text("Update Profile"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  _isEditing = false;
+                });
+              },
+              label: Text("Back"),
+              icon: Icon(Icons.arrow_back),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +151,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
           margin: EdgeInsets.only(top: 40),
           child: Consumer<AuthenticationProvider>(
             builder: (context, auth, _) {
-              String firstName = auth.userData?['firstName'];
-              String lastName = auth.userData?['lastName'];
-              String email = auth.userData?['email'];
-              String username = auth.userData?['username'];
+              String firstName = auth.userData?['firstName'] ?? '';
+              String lastName = auth.userData?['lastName'] ?? '';
+              String email = auth.userData?['email'] ?? '';
+              String username = auth.userData?['username'] ?? '';
 
               return Column(
                 spacing: 20,
@@ -37,31 +166,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       style: Theme.of(context).textTheme.displayLarge,
                     ),
                   ),
-                  const Image(
-                    image: AssetImage('assets/images/blank_avatar.webp'),
-                    width: 100,
-                  ),
-                  Text(
-                    '$firstName $lastName',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  Text(
-                    username,
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  Text('Email: $email'),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await Provider.of<AuthenticationProvider>(
-                        context,
-                        listen: false,
-                      ).signOut();
-                      if (context.mounted) {
-                        context.go('/login');
-                      }
-                    },
-                    child: const Text('Sign Out'),
-                  ),
+                  if (!_isEditing) ...[
+                    Text(
+                      '$firstName $lastName',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    Text(
+                      username,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text('Email: $email'),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _isEditing = true;
+                        });
+                      },
+                      child: Text("Edit Profile"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _deleteAccount();
+                      },
+                      child: Text("Delete Account"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await Provider.of<AuthenticationProvider>(
+                          context,
+                          listen: false,
+                        ).signOut();
+                        if (context.mounted) {
+                          context.go('/login');
+                        }
+                      },
+                      child: const Text('Sign Out'),
+                    ),
+                  ] else
+                    _buildEditProfileSection(),
                 ],
               );
             },
