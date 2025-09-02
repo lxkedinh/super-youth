bb608de1885ac24d12338751af34d49c4ac7619c
Luke Dinh <lukexdinh@gmail.com>
2025-09-01 22:23:38 -0700

    feat: nav drawer

---
+++ lib/widgets/NavDrawer.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20,
+        children: [
+          //onPressed serves as a function that responds to a specific input i.e. press
+          TextButton(
+            onPressed: () {
+              context.go('/home');
+            },
+            child: Text("Home", style: TextTheme.of(context).headlineMedium),
+          ),
+
+          TextButton(
+            onPressed: () {
+              context.go('/profile');
+            },
+            child: Text("Profile", style: TextTheme.of(context).headlineMedium),
+          ),
+        ],
+      ),
+    );
+  }
+}
