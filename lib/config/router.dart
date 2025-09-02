cd06c137ce3f0c934a7e933f0506d36a096ac399
Luke Dinh <lukexdinh@gmail.com>
2025-09-02 15:18:50 -0700

    fix: profile screen route path

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -2,23 +2,19 @@ import 'package:go_router/go_router.dart';
 import 'package:sunny_chen_project/screens/feedback_screen.dart';
 import 'package:sunny_chen_project/screens/home_screen.dart';
 import 'package:sunny_chen_project/screens/login_screen.dart';
-import 'package:sunny_chen_project/screens/profile_screen.dart';
 import 'package:sunny_chen_project/screens/sign_up_screen.dart';
 import 'package:sunny_chen_project/screens/splash_screen.dart';
 import 'package:sunny_chen_project/screens/try_screen.dart';
 import 'package:sunny_chen_project/screens/unit_screen.dart';
 
+import '../screens/profile_screen.dart';
+
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => SplashScreen()),
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => HomeScreen(),
-      routes: [
-        GoRoute(path: 'profile', builder: (context, state) => ProfileScreen()),
-      ],
-    ),
+    GoRoute(path: '/home', builder: (context, state) => HomeScreen()),
+    GoRoute(path: '/profile', builder: (context, state) => ProfileScreen()),
     GoRoute(path: '/login', builder: (context, state) => LoginScreen()),
     GoRoute(path: '/signup', builder: (context, state) => SignUpScreen()),
     GoRoute(
